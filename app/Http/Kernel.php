90ae77de71b99cb4ec483e2408afb20c2204068e
hamza umar <humar6078@gmail.com>
2022-08-25 21:36:14 +0000

    auth staff check fixed

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -53,6 +53,7 @@ class Kernel extends HttpKernel
      * @var array<string, class-string|string>
      */
     protected $routeMiddleware = [
+        'authstaffcheck' => \App\Http\Middleware\AuthCheck::class,
         'authcheck' => \App\Http\Middleware\AuthCheck::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,

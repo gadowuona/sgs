7d0e76f7740321ff67845da4c3cc438b5e505e12
Hamza Umar <humar6078@gmail.com>
2025-06-17 23:15:36 +0000

    Added docker to fix local version conflict

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,22 +14,25 @@
 </head>
 
 <body class="antialiased">
-    <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+    <div
+        class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
         @if (Route::has('login'))
-        <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
-            @auth
-            <a href="{{ url('/dashboard') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-            @else
-            <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
-            @endauth
-        </div>
+            <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                @auth
+                    <a href="{{ url('/dashboard') }}"
+                        class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                @else
+                    <a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Log in</a>
+                @endauth
+            </div>
         @endif
 
         <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
             <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
                 <!--  -->
                 <a href="route('dashboard')" title="UCC SGS" class="flex items-center">
-                    <img src="{{ asset('assets/logo.png') }}" alt="UCC" style=" height: 100px; margin-bottom: 5px;">
+                    <img src="{{ asset('assets/logo.png') }}" alt="UCC"
+                        style=" height: 100px; margin-bottom: 5px;">
                     <div id="site-slogan" class="text-gray-600 dark:text-[#EF3B2D] text-[2rem] font-semibold">UCC School
                         of Graduate
                         Studies <br>Supervisor
@@ -41,49 +44,39 @@
                 <div class="grid grid-cols-1 md:grid-cols-2">
                     <div class="p-6">
                         @if (Route::has('login'))
-                        <div class="">
-                            @auth
-                            <div class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
-                                </svg>
-                                <div class="ml-2 text-gray-600 dark:text-gray-400 text-xl leading-7 font-semibold">
-                                    <a href="{{ url('/dashboard') }} " class="underline text-gray-600 !uppercase">Dashboard</a>
-                                </div>
+                            <div class="">
+                                @auth
+                                    <div class="flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" fill="none"
+                                            viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                        </svg>
+                                        <div class="ml-2 text-gray-600 dark:text-gray-400 text-xl leading-7 font-semibold">
+                                            <a href="{{ url('/dashboard') }} "
+                                                class="underline text-gray-600 !uppercase">Dashboard</a>
+                                        </div>
+                                    </div>
+                                @else
+                                    <div class="flex items-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20"
+                                            fill="currentColor">
+                                            <path fill-rule="evenodd"
+                                                d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z"
+                                                clip-rule="evenodd" />
+                                        </svg>
+                                        <div class="ml-2 text-gray-600 dark:text-gray-400 text-xl leading-7 font-semibold">
+                                            <a href="{{ route('login') }}" class="underline text-gray-900 !uppercase">Log
+                                                in</a>
+                                        </div>
+                                    </div>
+                                @endauth
                             </div>
-                            @else
-                            <div class="flex items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
-                                    <path fill-rule="evenodd" d="M3 3a1 1 0 011 1v12a1 1 0 11-2 0V4a1 1 0 011-1zm7.707 3.293a1 1 0 010 1.414L9.414 9H17a1 1 0 110 2H9.414l1.293 1.293a1 1 0 01-1.414 1.414l-3-3a1 1 0 010-1.414l3-3a1 1 0 011.414 0z" clip-rule="evenodd" />
-                                </svg>
-                                <div class="ml-2 text-gray-600 dark:text-gray-400 text-xl leading-7 font-semibold">
-                                    <a href="{{ route('login') }}" class="underline text-gray-900 !uppercase">Log
-                                        in</a>
-                                </div>
-                            </div>
-                            @endauth
-                        </div>
                         @endif
                     </div>
 
                     <div class="p-6 border-t border-gray-200 dark:border-gray-700 md:border-t-0 md:border-l">
-                        <div class="flex items-center">
-                            <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                <path d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z">
-                                </path>
-                                <path d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"></path>
-                            </svg>
-                            <div class="ml-4 text-lg leading-7 font-semibold"><a href="https://laracasts.com" class="underline text-gray-900 dark:text-white">Laracasts</a></div>
-                        </div>
 
-                        <div class="ml-12">
-                            <div class="mt-2 text-gray-600 dark:text-gray-400 text-sm">
-                                Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript
-                                development. Check them out, see for yourself, and massively level up your
-                                development
-                                skills in the process.
-                            </div>
-                        </div>
                     </div>
 
                 </div>
